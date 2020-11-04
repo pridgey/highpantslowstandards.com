5ffef92081e61fd947f1293235573d1acca866e1
Taylor Pridgen <tpridgen@starrez.com>
2020-11-03 22:14:41 -0700

    main/ I might need this

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Layout = styled.main`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: sans-serif;
+  color: #FCFCFC;
+  background-color: #030303;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Layout>
+     <img alt="old-man" src="/man-image.jpg" width="750px" />
+     <h1>High Pants - Low Standards</h1>
+     <h2>Coming Soon</h2>
+   </Layout>
   );
 }
 
