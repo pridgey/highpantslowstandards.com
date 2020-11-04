a05265ec2c6dc7d2a3d1089ae18a90c35ef615f4
Taylor Pridgen <tpridgen@starrez.com>
2020-11-03 22:29:47 -0700

    main/ mobile shit

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,11 @@ const Layout = styled.main`
   background-image: url('harold.jpg');
   background-size: cover;
   background-position: center;
+
+  @media only screen and (max-width: 600px) {
+    font-size: 14px;
+    align-items: center;
+  }
 `;
 
 function App() {
